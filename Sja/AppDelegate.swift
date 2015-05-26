dab18f1d67b95f8931296a22ca68d1e966ddf251
omarkj <omarkj@gmail.com>
2015-05-25 22:48:43 -0700

    Initial Commit

---
+++ Sja/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sja
+//
+//  Created by Ómar Kjartan Yasin on 25/05/15.
+//  Copyright (c) 2015 Ómar Kjartan Yasin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SjaTests/SjaTests.swift
@@ -0,0 +1,36 @@
+//
+//  SjaTests.swift
+//  SjaTests
+//
+//  Created by Ómar Kjartan Yasin on 25/05/15.
+//  Copyright (c) 2015 Ómar Kjartan Yasin. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SjaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
